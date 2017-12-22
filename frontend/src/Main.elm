4645885eabcb6b2d8f2f7cffdd000cc3c00b0679
Juho Ylipoti <juho.ylipoti@zalando.de>
2017-12-22 17:25:00 +0100

    google sso

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,33 +1,32 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, img, button)
+import Html.Attributes exposing (src, class, attribute, id)
 
+port renderGoogleSSOButton : String -> Cmd msg
+port username : (String -> msg) -> Sub msg
 
 ---- MODEL ----
 
-
 type alias Model =
-    {}
+    {username: String}
 
 
-init : ( Model, Cmd Msg )
+init : (Model, Cmd Msg)
 init =
-    ( {}, Cmd.none )
-
+    (Model "", renderGoogleSSOButton "g-sso-button")
 
 
 ---- UPDATE ----
 
 
 type Msg
-    = NoOp
+    = NoOp | InitGoogleSSO
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     ( model, Cmd.none )
-
 
 
 ---- VIEW ----
@@ -38,12 +37,22 @@ view model =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
+        , div [id "g-sso-button"] [text "click me"]
+          div [id "g-sso-button-2", class ] [text "click me"]
         ]
+
+
+---- SUBSCRIPTIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    username
+
 
 
 
 ---- PROGRAM ----
-
 
 main : Program Never Model Msg
 main =
@@ -51,5 +60,5 @@ main =
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,30 @@ import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+var app = Main.embed(document.getElementById('root'));
 
 registerServiceWorker();
+
+app.ports.renderGoogleSSOButton.subscribe(function(id) {
+    renderGoogleSSOButton(id);
+});
+
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    app.ports.username.send(googleUser.getBasicProfile().getEmail());
+}
+function onFailure(error) {
+    console.log(error);
+}
+
+function renderGoogleSSOButton(id) {
+    gapi.signin2.render(id, {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
